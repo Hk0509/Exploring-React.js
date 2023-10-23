0b20a338b0d579f9374c28ed621899ca779b26c0
Hk0509 <80637998+Hk0509@users.noreply.github.com>
2023-10-23 22:38:14 +0530

    Added Google Signin and Signout

--- firebase-social-media-app/src/App.tsx
+++ firebase-social-media-app/src/App.tsx
@@ -1,8 +1,22 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Main } from "./Pages/main";
+import { Login } from "./Pages/login";
+import { Navbar } from "./Components/Navbar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
